33f4f696fecb9d2c8813907d29d76448140f8c90
Georgios Bakalski <george1216.gb@gmail.com>
2025-11-18 20:33:42 +0200

    Bookbot done

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from stats import  get_book_text, number_of_words, counting_characters, make_list
+import sys
+
+if len(sys.argv) != 2:
+	print ("Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
 
 def main():
-	text = get_book_text("books/frankenstein.txt")
+	book_path = sys.argv[1]
+	text = get_book_text(book_path)
 	chars = counting_characters(text)
 	sorted_chars = make_list(chars)
 	print ("============ BOOKBOT ============")
-	print ("Analyzing book found at books/frankenstein.txt...")
+	print (f"Analyzing book found at {book_path}...")
 	print ("----------- Word Count ----------")
 	print (f"Found {number_of_words(text)} total words")
 	print ("--------- Character Count -------")
